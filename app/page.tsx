1dd3c03fc4b82742ea49b57f68c2af5c9f968e56
Noel Sebu <noelsebu555@gmail.com>
2026-01-28 18:25:25 +0530

    removed navbar from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import CoreServices from './components/CoreServices'
+import Footer from './components/Footer'
+import CTASection from './components/CTASection'
 
 const page = () => {
   return (
     <div className='relative'>
-      <Navbar />
       <div className='pt-10'>
         <Hero />
         <CoreServices />
+        <CTASection />
+        <Footer />
       </div>
     </div>
   )
