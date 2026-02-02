06b38544191c4bae9e7048134327b46ca5c1b383
Noel Sebu <noelsebu555@gmail.com>
2026-02-02 21:56:32 +0530

    cta and service page added to service page

---
+++ app/services/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ServicesHeader from '../components/services/ServicesHeader'
+import CTASection from '../components/services/CTASection'
+
+const page = () => {
+  return (
+    <div>
+        <ServicesHeader />
+        <CTASection />
+    </div>
+  )
+}
+
+export default page
