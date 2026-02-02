a793da8a170fc3e5761aedab13d23cfb70b67c80
Noel Sebu <noelsebu555@gmail.com>
2026-02-02 21:55:11 +0530

    removed unnecessary comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import CoreServices from './components/CoreServices'
-import Footer from './components/Footer'
-import CTASection from './components/CTASection'
+"use client";
+import Hero from "./components/Hero";
+import CoreServices from "./components/CoreServices";
+import Footer from "./components/Footer";
+import CTASection from "./components/CTASection";
+import SpotlightBrandTwo from "./components/SpotlightBrandTwo";
 
 const page = () => {
   return (
-    <div className='relative'>
-      <div className=''>
+    <div className="relative">
+      <div>
+        <SpotlightBrandTwo />
         <Hero />
         <CoreServices />
         <CTASection />
         <Footer />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
