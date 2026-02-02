f18be4bf6a591fa2b69a951a47334f414b1ece5b
Noel Sebu <noelsebu555@gmail.com>
2026-02-02 23:16:46 +0530

    added value section and growht section

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import GrowthSection from '../components/about/GrowthSection'
+import ValuesSection from '../components/about/ValuesSection'
+
+const page = () => {
+  return (
+    <div>
+      <GrowthSection />
+      <ValuesSection />
+    </div>
+  )
+}
+
+export default page
